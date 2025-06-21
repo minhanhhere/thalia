9d015a753780f688ccf2f4d8237c4766994d646f
Martin Tran <matran@sgi.sk.ca>
2025-06-21 04:50:20 -0600

    use gulp to split html into partials

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const fileinclude = require('gulp-file-include');
+const browserSync = require('browser-sync').create();
+
+// File paths
+const paths = {
+  src: {
+    html: 'src/**/*.html',
+    partials: 'src/partials/**/*.html',
+  },
+  dist: './',
+};
+
+// HTML task - processes includes and copies to dist
+gulp.task('html', function () {
+  return gulp
+    .src(['src/*.html'])
+    .pipe(
+      fileinclude({
+        prefix: '@@',
+        basepath: '@file',
+      })
+    )
+    .pipe(gulp.dest(paths.dist))
+    .pipe(browserSync.stream());
+});
+
+// Browser Sync task - starts local server with live reload
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: paths.dist,
+    },
+    port: 3000,
+  });
+});
+
+// Watch task - watches for changes and triggers rebuilds
+gulp.task('watch', function () {
+  gulp.watch([paths.src.html, paths.src.partials], gulp.series('html'));
+});
+
+// Default task - runs all tasks and starts watching
+gulp.task('default', gulp.series('html', gulp.parallel('serve', 'watch')));
