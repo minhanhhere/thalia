e0ecf05d4de02b86362dc92883d63297968094ed
Martin Tran <matran@sgi.sk.ca>
2025-07-15 23:15:34 -0600

    add cv

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const config = {
   siteTitle: 'Ba Thien Thu Nguyen | My Portfolio',
   linkedin: 'https://www.linkedin.com/in/msthunguyen',
   email: 'thaliaincanada@gmail.com',
-  cv: 'https://cv',
+  cv: '/assets/cv.pdf',
 };
 
 // File paths
